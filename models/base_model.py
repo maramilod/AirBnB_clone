ee3875ae810e1ece0f42c8d804774d73563f927c
maramilod <marammilod03@gmail.com>
2023-12-04 19:10:11 +0000

    add task 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,19 @@ from datetime import datetime
 class BaseModel:
     """ starting of the class """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Public instance attributes """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            formated = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updeated_at":
+                    setattr(self, key, datetime.strptime(value, formated))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """ updates the public instance
